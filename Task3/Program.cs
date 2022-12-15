5b5f4a590b20b542ebd90fec939b34d27e1be8f8
Jacky_tut <netfinnov@mail.ru>
2022-12-15 21:05:58 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int numbers =  int.Parse(Console.ReadLine());
+int[] arr = new int[numbers];
+
+Console.WriteLine("Введите числа через пробел:");
+string[] arrayNumber =  Console.ReadLine().Split(' ');
+ 
+for (int i = 0; i < arrayNumber.Length; i++)
+{
+    arr[i] = int.Parse(arrayNumber[i]);
+    Console.Write($"{arr[i]}");
+}
+
+
+
+
