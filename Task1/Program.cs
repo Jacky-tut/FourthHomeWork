71c5e9645c1ba0b3835b7f939c11cc98cf5fee9d
Jacky_tut <netfinnov@mail.ru>
2022-12-15 19:11:16 +0300

    task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите число A: ");
+string numberOneStr = Console.ReadLine();
+Console.WriteLine("Введите число B: ");
+string numberTwoStr = Console.ReadLine();
+
+int numberA = Convert.ToInt32(numberOneStr);
+int numberB = Convert.ToInt32(numberTwoStr);
+int tmp = numberA;
+for (int i = 1; i <= numberB; i++)
+{
+    tmp = tmp * numberA;
+}
+
+double result = Math.Pow(numberA, numberB);
+Console.WriteLine($"{result}");
